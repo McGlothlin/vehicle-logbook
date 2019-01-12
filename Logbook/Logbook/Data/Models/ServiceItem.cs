89806d549dd2fb1106d568b122315e415f1d6f8b
Sean McGlothlin <sean.michael.mcglothlin@gmail.com>
2019-01-12 19:35:39 +0000

    init commit

---
+++ Logbook/Logbook/Data/Models/ServiceItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logbook.Data.Models
+{
+    public class ServiceItem
+    {
+        public long Id { get; set; }
+        public string Task { get; set; }
+        public string Interval { get; set; }
+    }
+}
---
+++ Logbook/Logbook/Data/Models/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logbook.Data.Models
+{
+    public class Vehicle
+    {
+        public long Id { get; set; }
+        public long OwnerId { get; set; }
+        public string Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string VIN { get; set; }
+        public string Mileage { get; set; }
+    }
+}
